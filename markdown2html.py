aa7d8364419e70cac10872158d9c8e7927a9c007
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-12-13 10:39:03 +0100

    start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,27 @@
+#!/usr/bin/python3
+"""
+markdown2html.py - Convert Markdown file to HTML file.
+
+Usage: ./markdown2html.py <input_file> <output_file>
+
+Arguments:
+<input_file>: Path to the input Markdown file.
+<output_file>: Path to the output HTML file.
+
+Examples:
+$ ./markdown2html.py
+Usage: ./markdown2html.py <input_file> <output_file>
+
+$ ./markdown2html.py bla.md
+Missing bla.md
+
+$ ./markdown2html.py bla.md blu.html
+"""
+
 import sys
 import os
 import markdown
+
 
 def convert_markdown_to_html(input_file, output_file):
     """
@@ -34,6 +55,7 @@ def convert_markdown_to_html(input_file, output_file):
     with open(output_file, 'w') as file:
         file.write(html_content)
 
+
 if __name__ == "__main__":
     # Ensure correct usage with two arguments
     if len(sys.argv) != 3:
@@ -42,6 +64,11 @@ if __name__ == "__main__":
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+
+    # Check if the input Markdown file exists
+    if not os.path.exists(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
 
     convert_markdown_to_html(input_file, output_file)
     sys.exit(0)
